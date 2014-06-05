a691d98b61dfa7b2f5ca401460c6788c8383613e
Jason Jones <jasonedwardjones@gmail.com>
2014-06-05 11:12:03 -0800

    Fix compile error and consolidate config

--- vaal.go
+++ vaal.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -9,14 +11,34 @@ import (
 )
 
 func main() {
-	administrator := administrator.NewAdministrator()
+	conf := loadConfigFromFile("./config.json")
+
+	administrator := api.NewAdministrator()
 	server := api.NewHTTPServer(administrator)
-	router := api.CreateRouter(server)
+	router, err := api.CreateRouter(server)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	http.Handle("/", router)
 
-	log.Printf("Vaal server started at http://%s\n", config.conf.URL)
-	err := http.ListenAndServe(config.conf.URL, nil)
+	log.Printf("Vaal server started at http://%s\n", conf.URL)
+	err = http.ListenAndServe(conf.URL, nil)
 	if err != nil {
 		fmt.Println(err)
 	}
+}
+
+type config struct {
+	URL string `json:"url"`
+}
+
+func loadConfigFromFile(path string) *config {
+	conf := &config{}
+	configFile, err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Printf("File error: %v\n", err)
+	}
+	json.Unmarshal(configFile, conf)
+	return conf
 }
